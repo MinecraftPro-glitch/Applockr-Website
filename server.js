514049b20943328f6545144bf58b6ef226294507
Massimiliano Veda <142025703+MinecraftPro-glitch@users.noreply.github.com>
2025-05-14 21:21:24 +0200

    Removed Useless Server Code

--- server.js
+++ server.js
@@ -2,19 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Serve static files (HTML, CSS, JS) from "public" folder
 app.use(express.static('public'));
-
-app.get('/run-code', (req, res) => {
-  console.log('Code is running!');
-  runMyCode();
-  res.send('Code executed successfully!');
-});
-
-function runMyCode() {
-  console.log('Running custom logic...');
-
-}
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
